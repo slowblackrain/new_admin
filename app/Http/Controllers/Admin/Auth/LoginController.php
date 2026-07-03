017b32d06474241c7c826ab98f19776aa3c7cd91
dometopia <slowblackrain@kakao.com>
2026-07-03 11:17:56 +0900

    fix: remove admin password bcrypt upgrade to preserve legacy system compatibility

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -29,11 +29,8 @@ class LoginController extends Controller
             return redirect()->intended(route('admin.dashboard'));
         }
 
-        // 2. Legacy Check (md5 or sha256(md5)) and upgrade to Bcrypt
+        // 2. Legacy Check (md5 or sha256(md5))
         if ($admin && (md5($credentials['mpasswd']) === $admin->mpasswd || hash('sha256', md5($credentials['mpasswd'])) === $admin->mpasswd)) {
-            $admin->mpasswd = \Illuminate\Support\Facades\Hash::make($credentials['mpasswd']);
-            $admin->save();
-            
             Auth::guard('admin')->login($admin);
             return redirect()->intended(route('admin.dashboard'));
         }
